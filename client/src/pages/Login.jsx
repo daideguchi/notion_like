c6c4d236a24ee46257f8560f64e9a611e4fa73f0
daideguchi <dd.1107.11107@gmail.com>
2022-11-10 22:42:02 +0900

    221110

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import AccessAlarmsIcon from "@mui/icons-material/AccessAlarms";
+
+function Login() {
+  return (
+      <div>Login 
+      </div>
+  )
+}
+
+export default Login
